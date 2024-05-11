60268e04663109a976d1194b66c371a73ed1e382
StringKe <authemail@qq.com>
2024-05-11 15:28:10 +0800

    feat: enhance TRPCReactProvider with PropsWithChildren, optimize imports

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { loggerLink, unstable_httpBatchStreamLink } from '@trpc/client';
 import { createTRPCReact } from '@trpc/react-query';
 import { type inferRouterInputs, type inferRouterOutputs } from '@trpc/server';
-import { useState } from 'react';
+import { useState, type PropsWithChildren } from 'react';
 import SuperJSON from 'superjson';
 
 import { type AppRouter } from '@/server/api/root';
@@ -37,7 +37,7 @@ export type RouterInputs = inferRouterInputs<AppRouter>;
  */
 export type RouterOutputs = inferRouterOutputs<AppRouter>;
 
-export function TRPCReactProvider(props: { children: React.ReactNode }) {
+export function TRPCReactProvider(props: PropsWithChildren) {
     const queryClient = getQueryClient();
 
     const [trpcClient] = useState(() =>
